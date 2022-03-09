640e3757dc6ee6af1a8c04f405e4cad3e52cffaf
glittering-code <99531828+glittering-code@users.noreply.github.com>
2022-03-09 17:17:54 +0530

    dummy file

---
+++ mini_project/file1.java
@@ -0,0 +1,7 @@
+class file1 {
+  public static void print() {
+    System.out.println("**********************");
+  }
+  public static void main(String [] args) {
+    System.out.println("hello world");
+}
