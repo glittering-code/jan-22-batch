f98c4d8f50c49040048968374ea03c0807af8437
glittering-code <99531828+glittering-code@users.noreply.github.com>
2022-03-09 17:18:25 +0530

    added more stars

--- mini_project/file1.java
+++ mini_project/file1.java
@@ -1,6 +1,6 @@
 class file1 {
   public static void print() {
-    System.out.println("**********************");
+    System.out.println("*************************");
   }
   public static void main(String [] args) {
     System.out.println("hello world");
